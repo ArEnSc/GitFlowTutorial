66bd361cdd3ab81d5d95d1d28e74c81a87e0de9e
michael <michael@polarbear.fish>
2019-01-14 23:45:32 -0500

    fucked up file

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def add():
 	print "ADD"
 def main():
 	print "HELLO"
+	print "WORLD"
+	print "fdsfdslkfdsklafd"
+####
+fdnsjal;fdnsjak;fmdksla;
+faskd'fklds;'afd
+faskdlfkdsl
+####
 
 if __name__ == '__main__':
 	main()
